4a9432d6b1118eefe6dd5460acb3b76af662eaa7
Nilton <cesar.n.2@hotmail.com>
2023-06-11 19:53:58 -0300

    first commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    Carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    Fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    Corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    Computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    Programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    Alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  };
